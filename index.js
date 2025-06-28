9cbce63cf95c743bebf50d17e65141bf25508eae
jahanvi rajpurohit <jahanvi.mailme@gmail.com>
2025-06-28 19:31:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js"
+const firebaseConfig = {
+    
+}
+const app = initializeApp(firebaseConfig)
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
